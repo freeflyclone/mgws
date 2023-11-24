e8289f849e5d030e20fd08efb37d1a78c21b7804
Evan Mortimore <evan-github@madstyle.net>
2023-11-24 14:30:01 -0800

    Add __FILENAME__ macro for TRACE(): just the file name, no long path

--- inc/mgws.h
+++ inc/mgws.h
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
-#define TRACE(...) {std::cerr << __FILE__ << ":" << __LINE__ << ", " << __VA_ARGS__ << std::endl;}
+#ifdef _WIN32
+	#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#elif __linux__
+	#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
+
+#define TRACE(...) {std::cerr << __FILENAME__ << ":" << __LINE__ << ", " << __VA_ARGS__ << std::endl;}
 
 #endif
