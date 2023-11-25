c8b0e66521d34802c7da7e76a8c2e68790c9fa27
Evan Mortimore <evan-github@madstyle.net>
2023-11-24 16:14:40 -0800

    Move remotes table code to ui.js

--- www/mgws/peer.js
+++ www/mgws/peer.js
@@ -1,6 +1,7 @@
 import { print } from "./index.js";
 import { ws } from "./websock.js";
 import { localStream, user_name_input, local_id_input } from "./local.js";
+import { OnSessionsChangedMessage } from "./ui.js";
 
 var configuration = {
 	iceServers: [
@@ -171,47 +172,6 @@ function OnTrackEvent(event) {
     if (remote_video.srcObject !== event.streams[0]) {
         remote_video.srcObject = event.streams[0];
     }
-}
-
-function OnTableRowOnClickEvent(event) {
-    var target = event.target;
-    var remoteId = null;
-    var remote_id_input = document.getElementById("remote_id_input");
-
-    if (target.className === "right") {
-        remoteId = target.previousSibling.innerHTML;
-    }
-    else {
-        remoteId = target.innerHTML;
-    }
-
-    remote_id_input.value = remoteId; 
-}
-
-export function OnSessionsChangedMessage(sessionsList) {
-    const  td_left = '<td class="left">';
-    const td_right = '<td class="right">';
-    const   td_end = '</td>';
-
-    // Overwrite "remotes-table" with new header (start from scratch)
-    var remotes_table = document.getElementById("remotes-table");
-    remotes_table.innerHTML = 
-        '<tr><th class="left">Remote ID:</th><th class="right">User Name</th></tr>' + 
-        '<tr class="short">' + td_left + '---------- ' + td_end + td_right + ' ----------' + td_end + '</tr>';
-
-    // Add 2 column HTML table row as child element of "remotes_table" for each session...
-    sessionsList.sessions.forEach(function(session) {
-        // Don't show ourselves in remotes table, that is non-sensical
-        if (session.localId === local_id_input.value)
-            return;
-
-        var tr = document.createElement('tr');
-
-        tr.className = "highlightable";
-        tr.innerHTML = td_left + session.localId + td_end + td_right + session.userName + td_end;
-        tr.addEventListener("click", OnTableRowOnClickEvent);
-        remotes_table.appendChild(tr);
-    });
 }
 
 function OnCallMessage(call) {
---
+++ www/mgws/ui.js
@@ -0,0 +1,42 @@
+
+function OnTableRowOnClickEvent(event) {
+    var target = event.target;
+    var remoteId = null;
+    var remote_id_input = document.getElementById("remote_id_input");
+
+    if (target.className === "right") {
+        remoteId = target.previousSibling.innerHTML;
+    }
+    else {
+        remoteId = target.innerHTML;
+    }
+
+    remote_id_input.value = remoteId; 
+}
+
+export function OnSessionsChangedMessage(sessionsList) {
+    const  td_left = '<td class="left">';
+    const td_right = '<td class="right">';
+    const   td_end = '</td>';
+
+    // Overwrite "remotes-table" with new header (start from scratch)
+    var remotes_table = document.getElementById("remotes-table");
+    remotes_table.innerHTML = 
+        '<tr><th class="left">Remote ID:</th><th class="right">User Name</th></tr>' + 
+        '<tr class="short">' + td_left + '---------- ' + td_end + td_right + ' ----------' + td_end + '</tr>';
+
+    // Add 2 column HTML table row as child element of "remotes_table" for each session...
+    sessionsList.sessions.forEach(function(session) {
+        // Don't show ourselves in remotes table, that is non-sensical
+        if (session.localId === local_id_input.value)
+            return;
+
+        var tr = document.createElement('tr');
+
+        tr.className = "highlightable";
+        tr.innerHTML = td_left + session.localId + td_end + td_right + session.userName + td_end;
+        tr.addEventListener("click", OnTableRowOnClickEvent);
+        remotes_table.appendChild(tr);
+    });
+}
+
