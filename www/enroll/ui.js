c6a81102a0842e3f393ddb8002ab4e17156d1b46
Evan Mortimore <evan-github@madstyle.net>
2023-12-07 18:56:50 -0800

    Add user_name_input handlers for per-key websock and "Enter" ends input to enroll UI

--- www/enroll/ui.js
+++ www/enroll/ui.js
@@ -1,4 +1,5 @@
 export var outputTextarea  = document.getElementById('output');
+export var user_name_input = document.getElementById("user_name_input");
 
 export function print(what) {
     if (outputTextarea) {
@@ -6,3 +7,17 @@ export function print(what) {
     }
 }
 
+user_name_input.addEventListener('input', (event) => {
+    // TODO: send every key via Websocket to server
+    // have server send true/false if current name is unique.
+});
+
+user_name_input.addEventListener('keyup', ({key}) => {
+    if (key === 'Enter') {
+        // TODO: register new user in server database
+        // if name longer than 0 characters and is unique.
+        localStorage.setItem('userName', user_name_input.value);
+        user_name_input.blur();
+        window.location.assign("/mgws")
+    }
+});
