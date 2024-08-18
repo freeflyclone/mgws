a0a89eaae68dd760d0f77cecda1cef5bfbcc0706
Evan Mortimore <evan-github@madstyle.net>
2024-08-18 22:53:58 +0000

    Add optional "rootDir" arg.  Better arg parsing is in order.

--- apps/dashboard/src/main.cpp
+++ apps/dashboard/src/main.cpp
@@ -5,13 +5,26 @@
 
 #include "sessionmgr.h"
 
-int main() {
+void usage() {
+    puts(
+        "dashboard "
+        "[rootDir] "
+    );
+}
+
+int main(int argc, char *argv[]) {
+    char _defaultRootDir[] = "../www/dashboard";
+    char* rootDir = _defaultRootDir; 
+
+    if (argc > 1)
+        rootDir = argv[1];
+
     TRACE(__FUNCTION__);
 
-    std::string root("../www/dashboard/");
+    std::string root(rootDir);
     std::string addr_port("https://0.0.0.0:443");
-    std::string cert_pem_file("/etc/letsencrypt/live/ws.lumenicious.com/fullchain.pem");
-    std::string key_pem_file("/etc/letsencrypt/live/ws.lumenicious.com/privkey.pem");
+    std::string cert_pem_file("/etc/letsencrypt/live/lumenicious.com/fullchain.pem");
+    std::string key_pem_file("/etc/letsencrypt/live/lumenicious.com/privkey.pem");
 
     auto sm = new SessionManager(root, addr_port, cert_pem_file, key_pem_file);
 
